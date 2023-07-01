70acf8589188ca76ce5f8aec466aa9859b320426
Stanislaw Shukan <shukans588@gmail.com>
2023-07-01 21:38:13 +0300

    Create database and models relationiships

---
+++ src/main/java/com/example/code/models/Measurement.java
@@ -0,0 +1,62 @@
+package com.example.code.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "measurements")
+public class Measurement {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "value")
+    @NotEmpty(message = "Temperature value should be not empty")
+    @Min(value = -100, message = "Temperature cannot be lower than -100 C")
+    @Max(value = 100, message = "Temperature cannot be greater than 100 C")
+    private double value;
+
+    @Column(name = "raining")
+    @NotEmpty(message = "Expression should be not empty")
+    private boolean raining;
+
+    @ManyToOne
+    @JoinColumn(name = "sensor_id",referencedColumnName = "id")
+    private Sensor sensor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public boolean isRaining() {
+        return raining;
+    }
+
+    public void setRaining(boolean raining) {
+        this.raining = raining;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+}
---
+++ src/main/java/com/example/code/models/Sensor.java
@@ -0,0 +1,47 @@
+package com.example.code.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+@Entity
+@Table(name = "sensor")
+public class Sensor {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Sensor name should not be empty")
+    private String name;
+
+    @OneToMany(mappedBy = "sensor")
+    private List<Measurement> measurements;
+
+    public Sensor(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Measurement> getMeasurements() {
+        return measurements;
+    }
+
+    public void setMeasurements(List<Measurement> measurements) {
+        this.measurements = measurements;
+    }
+}
