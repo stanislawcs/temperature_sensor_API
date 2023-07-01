df426dcac68e2f3d784db74266695c37f2dbc306
Stanislaw Shukan <shukans588@gmail.com>
2023-07-01 19:28:28 +0300

    first commit

---
+++ src/main/java/com/example/code/TemperatureSensorApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureSensorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemperatureSensorApiApplication.class, args);
+    }
+
+}
